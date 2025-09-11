e3869f801fd225684443903a7aa06d6780a49806
Rich107 <small.car3251@fastmail.com>
2025-09-11 14:43:31 +0100

    switch order

--- tick_mark.py
+++ tick_mark.py
@@ -10,6 +10,21 @@ time.sleep(0)
 # Get the current mouse position
 center_x, center_y = pyautogui.position()
 
+# Draw the tick mark
+tick_size = 80
+
+# Draw the first part of the tick (shorter line going down-left to center)
+pyautogui.moveTo(center_x - tick_size//2, center_y)
+pyautogui.mouseDown()
+pyautogui.moveTo(center_x - tick_size//4, center_y + tick_size//3)
+pyautogui.mouseUp()
+
+# Draw the second part of the tick (longer line going from center up-right)
+pyautogui.moveTo(center_x - tick_size//4, center_y + tick_size//3)
+pyautogui.mouseDown()
+pyautogui.moveTo(center_x + tick_size//2, center_y - tick_size//2)
+pyautogui.mouseUp()
+
 # Draw the face circle
 radius = 100
 pyautogui.moveTo(
@@ -24,21 +39,6 @@ for i in range(0, 361, 2):
     pyautogui.moveTo(x, y)
 
 pyautogui.PAUSE = 0.1
-pyautogui.mouseUp()
-
-# Draw the tick mark
-tick_size = 80
-
-# Draw the first part of the tick (shorter line going down-left to center)
-pyautogui.moveTo(center_x - tick_size//2, center_y)
-pyautogui.mouseDown()
-pyautogui.moveTo(center_x - tick_size//4, center_y + tick_size//3)
-pyautogui.mouseUp()
-
-# Draw the second part of the tick (longer line going from center up-right)
-pyautogui.moveTo(center_x - tick_size//4, center_y + tick_size//3)
-pyautogui.mouseDown()
-pyautogui.moveTo(center_x + tick_size//2, center_y - tick_size//2)
 pyautogui.mouseUp()
 
 print("Circle with tick mark drawn!")
